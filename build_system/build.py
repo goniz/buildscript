cce905005c2ef71a2decdc1d81d632a4e28c96d9
gz <goni1993@gmail.com>
2014-06-25 06:37:16 +0300

    added compile_link() more work around builder() should compile sample_c with compile() and link() just like compile_link()

--- build_system/build.py
+++ build_system/build.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python2
 
 from build_system.source import SourceDirectory
-from build_system.shell_command import ShellCommand
 from build_system.builder import Builder
+from build_system.toolchain import Toolchain
 
 
 def main():
     sources = SourceDirectory('sample_c', 'c').discover()
     print sources
+    #mips = Toolchain(prefix='/opt/mips-2013.11/bin/mips-linux-gnu-')
     builder = Builder(target='main', sources=sources, tmpdir='/tmp')
     builder.compile()
     builder.link()
+    builder.target = '/tmp/main2'
+    builder.compile_link()
 
 if '__main__' == __name__:
     main()
--- build_system/builder.py
+++ build_system/builder.py
@@ -13,27 +13,62 @@ class Builder(object):
         self.tmpdir = tmpdir
         self.language = language
         self.objects = []
+        self.flags = []
 
     @property
     def supported_source(self):
         return filter(lambda x: self.toolchain.language == x.language, self.sources)
 
+    def add_flag(self, flag):
+        if not flag in self.flags:
+            self.flags += [flag]
+
+    def remove_flag(self, flag):
+        if flag in self.flags:
+            self.flags.remove(flag)
+
     def compile(self):
         compiler = self.toolchain.compiler
         for source in self.supported_source:
             obj = os.path.join(self.tmpdir, source.filename.replace(source.extension, 'o'))
-            cmd = ShellCommand(compiler, [source.path, '-c', '-o', obj])
+            flags = [source.path]
+            flags += ['-c']
+            flags += ['-mtune=generic']
+            flags += ['-march=x86-64']
+            flags += ['-o']
+            flags += [obj]
+            cmd = ShellCommand(compiler, flags)
             print cmd.command
-            print cmd.run()
+            output = cmd.run()
+            if output:
+                print output
             self.objects += [obj]
 
     def link(self):
-        linkder = self.toolchain.linker
+        compiler = self.toolchain.compiler
         target = os.path.join(self.tmpdir, self.target)
         flags = self.objects
         flags += ['-o']
         flags += [target]
-        cmd = ShellCommand(linkder, flags)
+        cmd = ShellCommand(compiler, flags)
         print cmd.command
-        print cmd.run()
+        output = cmd.run()
+        if output:
+            print output
+
+    def compile_link(self):
+        compiler = self.toolchain.compiler
+        target = os.path.join(self.tmpdir, self.target)
+        flags = []
+        for source in self.supported_source:
+            flags += [source.path]
+        flags += ['-o']
+        flags += [target]
+        flags += ['-v']
+        cmd = ShellCommand(compiler, flags)
+        print cmd.command
+        output = cmd.run()
+        if output:
+            print output
+
 
